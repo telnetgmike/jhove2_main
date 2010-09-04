174131a8a7fabc2cb78b926158f2ae419e2657ee
Isaac Rabinovitch <isaac@picknit.com>
2010-09-03 17:26:50 -0700

    First pass at more useful Javadoc.

---
+++ src/main/java/org/jhove2/module/format/FormatModule.java
@@ -0,0 +1,114 @@
+/**
+ * JHOVE2 - Next-generation architecture for format-aware characterization
+ *
+ * Copyright (c) 2009 by The Regents of the University of California,
+ * Ithaka Harbors, Inc., and The Board of Trustees of the Leland Stanford
+ * Junior University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * o Neither the name of the University of California/California Digital
+ *   Library, Ithaka Harbors/Portico, or Stanford University, nor the names of
+ *   its contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.jhove2.module.format;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.util.List;
+
+import org.jhove2.annotation.ReportableProperty;
+import org.jhove2.core.JHOVE2;
+import org.jhove2.core.JHOVE2Exception;
+import org.jhove2.core.format.Format;
+import org.jhove2.core.source.Source;
+import org.jhove2.module.Module;
+
+/**
+ * Interface for format {@link Module} that models a
+ * format or format family.
+ * 
+ * @author mstrong, slabrams, smorrissey
+ */
+public interface FormatModule
+	extends Module
+{
+    /**
+     * Invoke the parsing of the source unit and validate all registered profiles.
+     * @param jhove2 JHOVE2 framework
+     * @param source Source to be parsed
+     * @throws JHOVE2Exception
+     * @see org.jhove2.module.Command#execute(org.jhove2.core.JHOVE2, org.jhove2.core.source.Source)
+     */
+    public void invoke(JHOVE2 jhove2, Source source)
+       throws JHOVE2Exception;
+       
+	/**
+	 * Parse a source unit.
+	 * 
+	 * @param jhove2
+	 *            JHOVE2 framework
+	 * @param source
+	 *            Source unit
+	 * @return Number of bytes consumed
+	 * @throws EOFException
+	 *             If End-of-File is reached reading the source unit
+	 * @throws IOException
+	 *             If an I/O exception is raised reading the source unit
+	 * @throws JHOVE2Exception
+	 */
+	public long parse(JHOVE2 jhove2, Source source)
+		throws EOFException, IOException, JHOVE2Exception;
+	
+	/**
+	 * Get format module format.
+	 * 
+	 * @return Format module format
+	 */
+	@ReportableProperty(order = 1, value = "Format module format.")
+	public Format getFormat();
+
+	/**
+	 * Get format module format profiles.
+	 * 
+	 * @return Format module format profiles.
+	 */
+	@ReportableProperty(order = 2, value = "Format module format profiles.")
+	public List<FormatProfile> getProfiles();
+
+	/** Set module format.
+	 * @param format Module format
+	 */
+	public void setFormat(Format format);
+	
+	/**
+	 * Add a format module format profile.
+	 * 
+	 * @param profiles
+	 *            Format module format profile
+	 */
+	public void setProfiles(List<FormatProfile> profiles);
+}
---
+++ src/main/java/org/jhove2/module/format/tiff/TiffIFD.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.jhove2.module.format.tiff;
+
+
+/**
+ * @author mstrong
+ *
+ */
+public class TiffIFD 
+extends IFD
+implements Comparable {
+    
+    
+    public static final int
+        DATETIME = 306;
+    
+    public TiffIFD() {
+        super();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
